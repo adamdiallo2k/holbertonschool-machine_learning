69d021ca85d874bd91f554e440fe9bd7f715e4f2
Adam Diallo <adamdiallo2k@gmail.com>
2023-12-06 11:55:17 -0800

    task 2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -15,15 +15,10 @@ def matrix_shape(matrix):
                     inner_inner_len = max(inner_inner_len, len(element))
 
     if inner_inner_len:  # This is a 3D matrix
-        return (outer_len, inner_len, inner_inner_len)
+        return [outer_len, inner_len, inner_inner_len]
     elif inner_len:  # This is a 2D matrix
-        return (outer_len, inner_len)
+        return [outer_len, inner_len]
     else:  # This is a 1D list
-        return (outer_len,)
+        return [outer_len]
 
-# Example usage
-matrix_2d = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-matrix_3d = [[[1, 2], [3, 4]], [[5, 6], [7, 8]], [[9, 10], [11, 12]]]
 
-print("Shape of 2D matrix:", matrix_shape(matrix_2d))  # Output will be (3, 3)
-print("Shape of 3D matrix:", matrix_shape(matrix_3d))  # Output will be (3, 2, 2)
