597cbc79139e7d990d09c0a9e3f2d3b71a992520
Adam Diallo <adamdiallo2k@gmail.com>
2023-12-06 11:52:11 -0800

    task 2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,29 +1,28 @@
-#!/usr/bin/env python3
-
-def compute_shape_3d(nested_list):
-    if not isinstance(nested_list, list) or not nested_list:
+def matrix_shape(matrix):
+    if not isinstance(matrix, list) or not matrix:
         return []
 
-    # Get the length of the outer list
-    outer_len = len(nested_list)
-
-    # Initialize dimensions for the inner lists
+    outer_len = len(matrix)
     inner_len = 0
     inner_inner_len = 0
 
-    # Iterate over each item in the outer list
-    for item in nested_list:
-        # Check if the item is a list itself
-        if isinstance(item, list):
-            inner_len = max(inner_len, len(item))
-            # Iterate over each item in the inner list
-            for inner_item in item:
-                if isinstance(inner_item, list):
-                    inner_inner_len = max(inner_inner_len, len(inner_item))
+    for row in matrix:
+        if isinstance(row, list):
+            inner_len = max(inner_len, len(row))
+            for element in row:
+                if isinstance(element, list):
+                    inner_inner_len = max(inner_inner_len, len(element))
 
-    # Return the shape as a tuple
-    return (outer_len, inner_len, inner_inner_len) if inner_inner_len else (outer_len, inner_len)
+    if inner_inner_len:  # This is a 3D matrix
+        return (outer_len, inner_len, inner_inner_len)
+    elif inner_len:  # This is a 2D matrix
+        return (outer_len, inner_len)
+    else:  # This is a 1D list
+        return (outer_len,)
 
 # Example usage
+matrix_2d = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 matrix_3d = [[[1, 2], [3, 4]], [[5, 6], [7, 8]], [[9, 10], [11, 12]]]
-print("Shape:", compute_shape_3d(matrix_3d))  # Output will be (3, 2, 2)
+
+print("Shape of 2D matrix:", matrix_shape(matrix_2d))  # Output will be (3, 3)
+print("Shape of 3D matrix:", matrix_shape(matrix_3d))  # Output will be (3, 2, 2)
