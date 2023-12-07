2b7f9be73136307eddc08d20bc229d66c854fdb9
Adam Diallo <adamdiallo2k@gmail.com>
2023-12-07 14:10:38 -0800

    task 14

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -8,7 +8,6 @@ import numpy as np
 def np_matmul(mat1, mat2):
     """
     Perform matrix multiplication on two numpy arrays.
-    
     :param mat1: First numpy array
     :param mat2: Second numpy array
     :return: Resultant numpy array after matrix multiplication
