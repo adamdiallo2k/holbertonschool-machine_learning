ad15549e6967cf002da4a9bf61a95dd2e935bccc
Adam Diallo <adamdiallo2k@gmail.com>
2023-12-07 09:28:58 -0800

    task 3

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -4,4 +4,5 @@
 
 def matrix_transpose(matrix):
     """commented function"""
-    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+    return [[matrix[j][i] for j in range(len(matrix))]
+            for i in range(len(matrix[0]))]
