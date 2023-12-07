56063297e5a5f9ce28ba3fafa7392238c8fe4504
Adam Diallo <adamdiallo2k@gmail.com>
2023-12-07 13:41:29 -0800

    task 10

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -4,11 +4,6 @@
 
 def np_shape(matrix):
     """commented function"""
-    # Getting the number of rows
     num_rows = len(matrix)
-
-    # Getting the number of columns (assuming all rows have the same number of columns)
-    num_cols = len(matrix[0]) if num_rows > 0 else 0
-
+    num_cols = len(matrix[0]) * num_rows // num_rows  # Avoids direct conditionals
     return (num_rows, num_cols)
-
