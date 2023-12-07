6148cf32c34ce5cdef03ad8f82bece0766f382fa
Adam Diallo <adamdiallo2k@gmail.com>
2023-12-07 14:09:15 -0800

    task 14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""commented module"""
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Perform matrix multiplication on two numpy arrays.
+    
+    :param mat1: First numpy array
+    :param mat2: Second numpy array
+    :return: Resultant numpy array after matrix multiplication
+    """
+    return np.matmul(mat1, mat2)
