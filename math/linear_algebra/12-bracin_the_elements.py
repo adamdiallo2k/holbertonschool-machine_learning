c517afc71b53b9e8dd5cb9c67a3572e034f29e63
Adam Diallo <adamdiallo2k@gmail.com>
2023-12-07 14:00:38 -0800

    task 12

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""commented module"""
+
+
+def np_elementwise(mat1, mat2):
+    """
+    Perform element-wise operations on two numpy arrays: addition, subtraction, 
+    multiplication, and division.
+
+    :param mat1: First numpy array
+    :param mat2: Second numpy array
+    :return: A tuple containing the element-wise sum, difference, product, and quotient
+    """
+    sum_array = mat1 + mat2  # Element-wise addition
+    diff_array = mat1 - mat2  # Element-wise subtraction
+    prod_array = mat1 * mat2  # Element-wise multiplication
+    quot_array = mat1 / mat2  # Element-wise division
+
+    return sum_array, diff_array, prod_array, quot_array
