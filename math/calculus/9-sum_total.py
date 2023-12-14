f9ea41826ecf411c0fba34b2335979429bb00233
Adam Diallo <adamdiallo2k@gmail.com>
2023-12-14 12:48:24 -0800

    task 10

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,10 +5,10 @@
 def summation_i_squared(n):
     """
     Calculate the sum of the squares of all integers from 1 to n.
-    The formula for the sum of the squares of 
+    The formula for the sum of the squares of
     the first n positive integers is n(n + 1)(2n + 1)/6.
     :param n: integer, the stopping condition
-    :return: integer, the sum of the squares 
+    :return: integer, the sum of the square
     from 1 to n, or None if input is not valid
     """
     if not isinstance(n, int) or n < 1:
