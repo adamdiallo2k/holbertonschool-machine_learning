a5df45c5409440b8c12d761bc55ba6871a72d5c5
Adam Diallo <adamdiallo2k@gmail.com>
2023-12-14 12:54:14 -0800

    task 10

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -3,14 +3,20 @@
 
 
 def poly_derivative(poly):
-    """commented function"""
+    """
+    Calculate the derivative of a polynomial represented by a list of coefficients.
+
+    :param poly: List of coefficients, where the index represents the power of x.
+    :return: List of coefficients representing the derivative of the polynomial.
+    """
     if not isinstance(poly, list) or not all(isinstance(coef, (int, float)) for coef in poly):
         return None
-    if len(poly) <= 1:  # This covers the case where the polynomial is a constant or empty
+
+    if len(poly) <= 1:  # The polynomial is constant or empty
         return [0]
-    
+
     # Calculate the derivative coefficients
-    derivative = [coef * power for power, coef in enumerate(poly) if power > 0]
-    
-    # The last term of the polynomial (highest power) will always have a derivative of 0 since it's constant after differentiation
-    return derivative[::-1]  # Reverse the list to represent the polynomial correctly
+    # Skip the first element (constant term) and use enumerate to get index and coefficient
+    derivative = [coef * index for index, coef in enumerate(poly) if index > 0]
+
+    return derivative
