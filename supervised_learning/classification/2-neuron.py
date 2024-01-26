fe2c3977b3d81895f47a63ba3e32fc0554d34326
Adam Diallo <adamdiallo2k@gmail.com>
2024-01-26 01:23:46 -0800

    task 2

--- supervised_learning/classification/2-neuron.py
+++ supervised_learning/classification/2-neuron.py
@@ -74,6 +74,5 @@ class Neuron:
         The cost of the model.
         """
         m = Y.shape[1]
-        cost = -(1 / m) * np.sum(Y * np.log(A)+ (1 - Y) * 
-        np.log(1.0000001 - A))
+        cost = -(1 / m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
         return cost
