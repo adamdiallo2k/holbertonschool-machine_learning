b3f923e7888fd876aee229704ce022674d33cf06
Adam Diallo <adamdiallo2k@gmail.com>
2024-03-01 01:20:14 -0800

    task 0

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -5,21 +5,21 @@
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 
+
 def create_placeholders(nx, classes):
     """
     Creates and returns two placeholders, x and y, for a neural network.
-    
+
     Arguments:
     nx -- scalar, size of an image vector (num_px * num_px = nx)
     classes -- scalar, number of classes (from 0 to classes-1)
-    
+
     Returns:
     x -- placeholder for the data input, of shape [None, nx] and dtype "float"
     y -- placeholder for the input labels, of shape [None, classes] and dtype "float"
     """
-    
+
     x = tf.placeholder(tf.float32, shape=[None, nx], name="x")
     y = tf.placeholder(tf.float32, shape=[None, classes], name="y")
-    
-    return x, y
 
+    return x, y
