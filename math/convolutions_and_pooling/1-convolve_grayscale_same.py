0bb75636bfa2cee9ffcdc835d23d06b1f5154820
Adam Diallo <adamdiallo2k@gmail.com>
2024-03-27 12:16:21 -0700

    task 0

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python3
+"""
+    Same Convolution
+"""
+
 import numpy as np
 
 
-def convolve_grayscale_valid(images, kernel):
+def convolve_grayscale_same(images, kernel):
     """
-        Function that performs a valid convolution on grayscale images
+        Function that performs a same convolution on grayscale images
 
         :param images: ndarray, shape(m, h, w), multiple grayscale images
         :param kernel: ndarray, shape(kh,kw), kernel for convolution
@@ -15,17 +20,26 @@ def convolve_grayscale_valid(images, kernel):
     kh, kw = kernel.shape
 
     # output size
-    output_height = h - kh + 1
-    output_width = w - kw + 1
+    output_height = h
+    output_width = w
 
     # initialize output
     convolved_images = np.zeros((m, output_height, output_width))
+
+    # calcul padding (size odd or even)
+    padding_width = int(kw / 2)
+    padding_height = int(kh / 2)
+
+    # add zero padding to the input images
+    image_pad = np.pad(images,
+                       ((0, 0), (padding_height, padding_height),
+                        (padding_width, padding_width)), mode='constant')
 
     # convolution
     for i in range(output_height):
         for j in range(output_width):
             # extract region from each image
-            image_zone = images[:, i:i+kh, j:j+kw]
+            image_zone = image_pad[:, i:i+kh, j:j+kw]
 
             # element wize multiplication
             convolved_images[:, i, j] = np.sum(image_zone * kernel,
