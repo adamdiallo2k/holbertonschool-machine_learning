603f4c42255e0bccd60c42f1c1277c8ff750e0b9
Adam Diallo <adamdiallo2k@gmail.com>
2024-03-27 03:51:16 -0700

    task 0

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Commented module"""
+"""Module commenté pour calculer le coût d'un réseau avec régularisation L2."""
 
 
 import numpy as np
@@ -7,26 +7,21 @@ import numpy as np
 
 def l2_reg_cost(cost, lambtha, weights, L, m):
     """
-    Calcule le coût d'un réseau de neurones avec régularisation L2.
+    Calcule le coût avec régularisation L2 d'un réseau de neurones.
 
-    Arguments:
-    cost -- le coût du réseau sans régularisation L2
-    lambtha -- le paramètre de régularisation
-    weights -- un dictionnaire des poids et biais (numpy.ndarrays) du réseau de neurones
-    L -- le nombre de couches dans le réseau de neurones
-    m -- le nombre de points de données utilisés
+    Arguments :
+    cost -- Coût sans régularisation L2
+    lambtha -- Paramètre de régularisation
+    weights -- Dictionnaire de poids et biais du réseau
+    L -- Nombre de couches dans le réseau
+    m -- Nombre de données utilisées
 
-    Returns:
-    Le coût du réseau en prenant en compte la régularisation L2.
+    Returns :
+    Coût total avec régularisation L2.
     """
-
-    l2_penalty = 0
-    for layer in range(1, L + 1):
-        # La clé pour les poids de chaque couche suit le format 'Wl', où layer est le numéro de la couche
-        W = weights['W' + str(layer)]
-        # Additionne la norme de Frobenius des poids (somme des carrés des éléments) pour chaque couche
-        l2_penalty += np.sum(np.square(W))
-
-    # Calcul du coût avec régularisation L2
+    
+    l2_penalty = sum([np.sum(np.square(weights['W' + str(l)]))
+                      for l in range(1, L + 1)])
+    
     cost_l2 = cost + (lambtha / (2 * m)) * l2_penalty
     return cost_l2
