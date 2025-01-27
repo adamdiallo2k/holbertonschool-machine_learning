7b2b026a26f805b7fa0bbee9252a08ff43cc384d
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2025-01-27 10:03:04 +0100

    Update 6-dropout_create_layer.py

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -1,38 +1,35 @@
 #!/usr/bin/env python3
 """
-    Create layer with Dropout regularization
+    Create layer with L2 regularization
 """
 
 import tensorflow.compat.v1 as tf
 
 
-def dropout_create_layer(prev, n, activation, keep_prob):
+def l2_reg_create_layer(prev, n, activation, lambtha):
     """
-        function that creates layer of NN using dropout
+        Function that creates a tensorflow layer includes L2 regularization
 
-        :param prev: tensor, output prev layer
-        :param n: number of nodes new layer
-        :param activation: activation function on the new layer
-        :param keep_prob: proba that node will be kept
+        :param prev: tensor, output of previous layer
+        :param n: number of nodes the new layer
+        :param activation: activation function used on the layer
+        :param lambtha: L2 regularization parameter
 
         :return: output of the new layer
     """
-
-    # define layer Dropout
-    dropout_layer = tf.compat.v1.layers.Dropout(rate=keep_prob)
-
     # set initialization to He et. al
     initializer = tf.keras.initializers.VarianceScaling(scale=2.0,
                                                         mode='fan_avg')
 
-    # apply dropout
+    # create layer Dense with parameters
     new_layer = (
         tf.layers.Dense(n,
                         activation=activation,
                         kernel_initializer=initializer,
-                        kernel_regularizer=dropout_layer,
+                        kernel_regularizer=tf.keras.regularizers.l2(lambtha),
                         name="layer"))
 
+    # apply layer to input
     output = new_layer(prev)
 
     return output
