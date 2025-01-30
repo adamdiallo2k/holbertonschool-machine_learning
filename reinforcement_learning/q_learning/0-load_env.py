e6fe8c634f734ab17bb6fbed8f7118a9434d10bf
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2025-01-30 11:30:00 +0100

    Update 0-load_env.py

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 """
-    Module pour charger l'environnement FrozenLake depuis OpenAI Gym.
+    Module pour charger l'environnement FrozenLake depuis OpenAI Gymnasium.
 """
-import gym
+import gymnasium as gym  # Mise à jour de l'importation pour Gymnasium
 
 
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
     """
-        Charge l'environnement FrozenLakeEnv depuis OpenAI Gym.
+        Charge l'environnement FrozenLakeEnv depuis OpenAI Gymnasium.
 
         :param desc: None ou liste représentant une carte personnalisée.
         :param map_name: None ou str indiquant le nom d'une carte préexistante.
         :param is_slippery: bool, définit si la glace est glissante (True) ou non (False).
 
-        :return: l'environnement Gym correspondant.
+        :return: l'environnement Gymnasium correspondant.
     """
-    env = gym.make('FrozenLake-v0',
+    env = gym.make('FrozenLake-v1',  # Gymnasium utilise 'FrozenLake-v1' au lieu de 'FrozenLake-v0'
                    desc=desc,
                    map_name=map_name,
                    is_slippery=is_slippery)
