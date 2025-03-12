fa39c6b901102fdaf124b19caf11c851592d02b9
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2025-03-12 16:24:21 +0100

    Update 0-load_env.py

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -1,30 +1,25 @@
 #!/usr/bin/env python3
 """
-    Module to load environment frozen-lake from gymnasium
+Module to load the FrozenLake environment from gymnasium
 """
-try:
-    import gymnasium as gym
-except ImportError:
-    import gym
+import gymnasium as gym
+
 
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
     """
-        load the premade FrozenLakeEnv env from OpenAI's gym
-        :param desc: None or list custom description of the map to load
-        :param map_name: None or string pre-made map to load
-        :param is_slippery: bool, determine if the ice is slippery
-        :return: the env
+    Load the pre-made FrozenLakeEnv environment from gymnasium
+    
+    Parameters:
+        desc: None or list of lists containing a custom description of the map
+        map_name: None or string containing the pre-made map to load
+        is_slippery: boolean to determine if the ice is slippery
+    
+    Returns:
+        The environment
     """
-    try:
-        # Try with newer versions
-        env = gym.make('FrozenLake-v1',
-                      desc=desc,
-                      map_name=map_name,
-                      is_slippery=is_slippery)
-    except:
-        # Fallback to older version
-        env = gym.make('FrozenLake-v0',
-                      desc=desc,
-                      map_name=map_name,
-                      is_slippery=is_slippery)
+    env = gym.make('FrozenLake-v1', 
+                   desc=desc,
+                   map_name=map_name,
+                   is_slippery=is_slippery)
+    
     return env
