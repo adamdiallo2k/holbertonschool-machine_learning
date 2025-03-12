8901d09f9d1c1f9ff94a3fd8cff4a88c781535b3
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2025-03-12 16:34:25 +0100

    Update 0-load_env.py

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -8,12 +8,12 @@ import gymnasium as gym
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
     """
     Load the pre-made FrozenLakeEnv environment from gymnasium
-    
+
     Parameters:
         desc: None or list of lists containing a custom description of the map
         map_name: None or string containing the pre-made map to load
         is_slippery: boolean to determine if the ice is slippery
-    
+
     Returns:
         The environment
     """
@@ -21,5 +21,5 @@ def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
                    desc=desc,
                    map_name=map_name,
                    is_slippery=is_slippery)
-    
+
     return env
