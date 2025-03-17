ad5c070bbf112306ade6ad871163c4ab32b1d8ff
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2025-03-17 11:18:59 +0100

    Update 0-dataset.py

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -17,33 +17,34 @@ class Dataset:
         Creates instance attributes
         """
         # Load datasets as supervised
-        self.data_train = tfds.load('ted_hrlr_translate/pt_to_en',
-                                    split='train', as_supervised=True)
-        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
-                                    split='validation', as_supervised=True)
-        
-        # Create tokenizers from the training set
-        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
-            self.data_train)
+        self.data_train = tfds.load(
+            'ted_hrlr_translate/pt_to_en',
+            split='train',
+            as_supervised=True
+        )
+        self.data_valid = tfds.load(
+            'ted_hrlr_translate/pt_to_en',
+            split='validation',
+            as_supervised=True
+        )
+
+        # Create tokenizers (pretrained) from Hugging Face
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(self.data_train)
 
     def tokenize_dataset(self, data):
         """
-        Creates sub-word tokenizers for our dataset
+        Loads pretrained sub-word tokenizers for our dataset.
+
         Args:
-            data: tf.data.Dataset whose examples are formatted as a tuple (pt, en)
+            data: tf.data.Dataset whose examples are formatted as (pt, en)
+
         Returns:
             tokenizer_pt: Portuguese tokenizer
             tokenizer_en: English tokenizer
         """
-        # Initialize the Portuguese tokenizer with pre-trained model
-        tokenizer_pt = BertTokenizerFast.from_pretrained(
-            'neuralmind/bert-base-portuguese-cased')
-        
-        # Initialize the English tokenizer with pre-trained model
-        tokenizer_en = BertTokenizerFast.from_pretrained(
-            'bert-base-uncased')
-        
-        # Train the tokenizers with maximum vocabulary size of 2^13
-        vocab_size = 2**13
-        
+        # Portuguese tokenizer from neuralmind
+        tokenizer_pt = BertTokenizerFast.from_pretrained("neuralmind/bert-base-portuguese-cased")
+        # English tokenizer
+        tokenizer_en = BertTokenizerFast.from_pretrained("bert-base-uncased")
+
         return tokenizer_pt, tokenizer_en
