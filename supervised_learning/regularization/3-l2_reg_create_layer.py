ca31817ae8bbb09ae199546843eae160ce33779f
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2025-03-17 11:32:52 +0100

    Update 3-l2_reg_create_layer.py

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -13,11 +13,11 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     Args:
         prev: a tensor containing the output of the previous layer
         n: the number of nodes in the new layer
-        activation: the activation function to be used
+        activation: the activation function that should be used on the layer
         lambtha: the L2 regularization parameter (float)
 
     Returns:
-        The output tensor of the new layer
+        The output of the new layer
     """
     # Define the L2 regularization
     l2_regularizer = tf.keras.regularizers.l2(lambtha)
@@ -27,8 +27,9 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
         units=n,
         activation=activation,
         kernel_regularizer=l2_regularizer,
-        # Optionally, specify a kernel initializer (e.g., VarianceScaling)
-        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0, mode='fan_avg')
+        kernel_initializer=tf.keras.initializers.VarianceScaling(
+            scale=2.0,
+            mode='fan_avg'
+        )
     )
-
     return layer(prev)
