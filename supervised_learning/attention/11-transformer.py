c4a08cecaed7d622d0a705eaa4350594236a8281
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2025-03-17 11:40:50 +0100

    Update 11-transformer.py

--- supervised_learning/attention/11-transformer.py
+++ supervised_learning/attention/11-transformer.py
@@ -1,67 +1,57 @@
 #!/usr/bin/env python3
 """
-Defines the Transformer network
+    Transformer
 """
 import tensorflow as tf
-# Import the required Encoder and Decoder
+
 Encoder = __import__('9-transformer_encoder').Encoder
 Decoder = __import__('10-transformer_decoder').Decoder
 
 
-class Transformer(tf.keras.Model):
+class Transformer(tf.keras.layers.Layer):
     """
-    Creates a Transformer network
+        class Transformer
     """
-    def __init__(self, N, dm, h, hidden, 
-                 input_vocab, target_vocab,
-                 max_seq_input, max_seq_target,
-                 drop_rate=0.1):
-        """
-        Class constructor
-        Args:
-            N: number of blocks in the encoder and decoder
-            dm: dimensionality of the model
-            h: number of heads
-            hidden: number of hidden units in the fully connected layers
-            input_vocab: size of the input vocabulary
-            target_vocab: size of the target vocabulary
-            max_seq_input: maximum sequence length possible for the input
-            max_seq_target: maximum sequence length possible for the target
-            drop_rate: dropout rate
-        Public instance attributes:
-            encoder - the encoder layer
-            decoder - the decoder layer
-            linear - a final Dense layer with target_vocab units
-        """
-        super(Transformer, self).__init__()
-        self.encoder = Encoder(N, dm, h, hidden,
-                               input_vocab, max_seq_input,
-                               drop_rate)
-        self.decoder = Decoder(N, dm, h, hidden,
-                               target_vocab, max_seq_target,
-                               drop_rate)
-        self.linear = tf.keras.layers.Dense(target_vocab)
 
-    def call(self, inputs, target, training,
-             encoder_mask, look_ahead_mask, decoder_mask):
+    def __init__(self, N, dm, h, hidden, input_vocab, target_vocab,
+                 max_seq_input, max_seq_target, drop_rate=0.1):
         """
-        Performs a forward pass through the Transformer.
-        Args:
-            inputs: tensor of shape (batch, input_seq_len) with the inputs
-            target: tensor of shape (batch, target_seq_len) with the target
-            training: boolean, whether the model is in training mode
-            encoder_mask: the padding mask to apply to the encoder
-            look_ahead_mask: the look-ahead mask to apply to the decoder
-            decoder_mask: the padding mask to apply to the decoder
-        Returns:
-            A tensor of shape (batch, target_seq_len, target_vocab)
-            containing the Transformer output
+            class init
+
+        :param N: number blocks in encoder & decoder
+        :param dm: dimensionality of the model
+        :param h: number of heads
+        :param hidden: number hidden units in fully connected layer
+        :param input_vocab: size input vocab
+        :param target_vocab: size target vocab
+        :param max_seq_input: max seq length for input
+        :param max_seq_target: max seq length for target
+        :param drop_rate: dropout rate
         """
-        # Pass inputs through the encoder
-        enc_output = self.encoder(inputs, training, encoder_mask)
-        # Pass the target and encoder output through the decoder
-        dec_output = self.decoder(target, enc_output, training,
-                                  look_ahead_mask, decoder_mask)
-        # Final linear projection
-        final_output = self.linear(dec_output)  # (batch, target_seq_len, target_vocab)
-        return final_output
+        super().__init__()
+        self.encoder = Encoder(N, dm, h, hidden, input_vocab,
+                               max_seq_input, drop_rate)
+        self.decoder = Decoder(N, dm, h, hidden, target_vocab,
+                               max_seq_target, drop_rate)
+        self.linear = tf.keras.layers.Dense(units=target_vocab)
+
+    def call(self, inputs, target, training, encoder_mask,
+             look_ahead_mask, decoder_mask):
+        """
+            call function
+
+        :param inputs: tensor, shape(batch,input_seq_len) inputs
+        :param target: tensor, shape(batch,target_seq_len) target
+        :param training: bool, determine if model training
+        :param encoder_mask: mask apply to encoder
+        :param look_ahead_mask: mask apply to decoder
+        :param decoder_mask: padding mask apply to the decoder
+
+        :return: tensor, shape(batch,target_seq_len,target_vocab)
+            transformer output
+        """
+        x = self.encoder(inputs, training, encoder_mask)
+        x = self.decoder(target, x, training, look_ahead_mask, decoder_mask)
+        output = self.linear(x)
+
+        return output
