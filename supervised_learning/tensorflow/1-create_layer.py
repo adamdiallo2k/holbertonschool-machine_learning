cb61f1213c16fa106e7cbf87cff69aa23c18df5d
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2025-03-17 11:51:40 +0100

    Update 1-create_layer.py

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python3
-"""Commented module """
+"""
+Module 1-create_layer
 
+Provides a function `create_layer` that builds a layer for the neural network.
+"""
 
 import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
+
+tf.disable_eager_execution()
 
 
 def create_layer(prev, n, activation):
     """
-    Creates a new TensorFlow layer.
+    Creates a TensorFlow layer using tf.keras.initializers.VarianceScaling with 
+    mode 'fan_avg' for the kernel weights.
 
-    Arguments:
-    prev -- tensor containing the output of the previous layer
-    n -- integer, number of nodes in the layer to create
-    activation -- the activation function to be used on the layer
+    Args:
+        prev (tensor): Tensor output of the previous layer.
+        n (int): Number of nodes in the layer to create.
+        activation (function): Activation function to be used in the layer.
 
     Returns:
-    The tensor output of the layer.
+        tensor: The tensor output of the created layer.
     """
-
-    # Define the He et al. initializer for the layer weights
     initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
-
-
-    layer = tf.layers.Dense(units=n, activation=activation,
-        kernel_initializer=initializer,
-        name="layer")
-
+    layer = tf.layers.Dense(units=n,
+                            activation=activation,
+                            kernel_initializer=initializer,
+                            name="layer")
     return layer(prev)
