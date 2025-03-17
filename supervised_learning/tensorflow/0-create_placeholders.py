83b50e30820b6a52c59be7a5978e24034efa4525
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2025-03-17 11:49:24 +0100

    Update 0-create_placeholders.py

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+Module 0-create_placeholders
+Provides a function `create_placeholders` that returns two TensorFlow placeholders.
+"""
+
 import tensorflow.compat.v1 as tf
 
 tf.disable_eager_execution()
@@ -9,9 +14,13 @@ def create_placeholders(nx, classes):
     Returns two placeholders, x and y, for a neural network:
       - x is placeholder for the input data to the network
       - y is placeholder for the one-hot labels
-    :param nx: number of feature columns in the data
-    :param classes: number of classes in the classifier
-    :return: placeholders x and y
+
+    Args:
+        nx (int): number of feature columns in our data
+        classes (int): number of classes in our classifier
+
+    Returns:
+        tuple: placeholders named x and y, respectively
     """
     x = tf.placeholder(tf.float32, shape=(None, nx), name="x")
     y = tf.placeholder(tf.float32, shape=(None, classes), name="y")
