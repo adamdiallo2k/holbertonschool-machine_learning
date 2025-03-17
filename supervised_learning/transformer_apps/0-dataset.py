216bd6fef545af5b1a927c063d77c16217f7450e
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2025-03-17 11:17:08 +0100

    Update 0-dataset.py

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -35,15 +35,6 @@ class Dataset:
             tokenizer_pt: Portuguese tokenizer
             tokenizer_en: English tokenizer
         """
-        # Extract the Portuguese and English sentences from the dataset
-        pt_texts = []
-        en_texts = []
-        
-        # Take all examples from the dataset for training the tokenizers
-        for pt, en in data:
-            pt_texts.append(pt.numpy().decode('utf-8'))
-            en_texts.append(en.numpy().decode('utf-8'))
-
         # Initialize the Portuguese tokenizer with pre-trained model
         tokenizer_pt = BertTokenizerFast.from_pretrained(
             'neuralmind/bert-base-portuguese-cased')
@@ -54,9 +45,5 @@ class Dataset:
         
         # Train the tokenizers with maximum vocabulary size of 2^13
         vocab_size = 2**13
-        
-        # For BERT tokenizers, we need to resize the vocabulary to match requirements
-        tokenizer_pt.resize_token_embeddings(vocab_size)
-        tokenizer_en.resize_token_embeddings(vocab_size)
         
         return tokenizer_pt, tokenizer_en
