7df15066a446807e0811b3849d9471c6a642d218
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2025-03-19 10:24:27 +0100

    Update 0-monte_carlo.py

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -1,55 +1,61 @@
 #!/usr/bin/env python3
 """
-This is the Temporal difference Project
-it use the FrozenLake8x8-v1 environment
-beware, do not slide on the hole
-By Ced
+Monte Carlo algorithm
 """
 import numpy as np
 
 
-def sample_episode(env, policy, max_steps=100):
+def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1,
+                gamma=0.99):
     """
-    Jouer un episode entier
+    Performs the Monte Carlo algorithm for estimating the value function.
+
+    Parameters:
+        env: Environment instance.
+        V: numpy.ndarray of shape (s,) containing the value estimates.
+        policy: Function that takes a state and returns the next action
+            to take.
+        episodes: Total number of episodes to train over.
+        max_steps: Maximum number of steps per episode.
+        alpha: Learning rate.
+        gamma: Discount rate.
+
+    Returns:
+        Updated value estimates V.
     """
-
-    SAR_list = []
-    observation = 0  # le jouer debute en haut a gauche
-    env.reset()
-    for j in range(max_steps):
-
-        action = policy(observation)
-
-        new_obs, reward, done, truncated, _ = env.step(action)
-        SAR_list.append((observation, reward))
-
-        if done or truncated:
-            break
-
-        observation = new_obs
-    return SAR_list
-
-
-def monte_carlo(env, V, policy, episodes=5000,
-                max_steps=100, alpha=0.1, gamma=0.99):
-    """
-    Utilise  Monte carlo, pour estimer la fonction de valeur
-    """
-
     for episode in range(episodes):
-        # reset the environment and sample one episode
-        SAR_list = sample_episode(env, policy, max_steps)
-        SAR_list = np.array(SAR_list, dtype=int)
+        # reset the environment and get initial state
+        state = env.reset()[0]
+        episode_data = []
+
+        for step in range(max_steps):
+            # select action based on policy
+            action = policy(state)
+
+            # take action
+            next_state, reward, terminated, truncated, _ = env.step(
+                action)
+
+            # Append state and reward to the episode history
+            episode_data.append((state, reward))
+
+            if terminated or truncated:
+                break
+
+            # move to the next state
+            state = next_state
 
         G = 0
-        for state, reward in reversed(SAR_list):
-            # return apres la fin de l'episode
+        episode_data = np.array(episode_data, dtype=int)
+
+        # Compute the returns for each state in the episode
+        for state, reward in reversed(episode_data):
+            # calculate this episode's return
             G = reward + gamma * G
-            #print("G", G)
-            print("V", V[state])
-            # attention, si l'etat est nouveau ?!
-            if state not in SAR_list[:episode, 0]:
+
+            # if this is a novel state
+            if state not in episode_data[:episode, 0]:
                 # Update the value function V(s)
                 V[state] = V[state] + alpha * (G - V[state])
-    env.close()
+
     return V
