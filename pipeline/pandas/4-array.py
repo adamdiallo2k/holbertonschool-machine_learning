c3cec8420bc2aa79070e0d36eb296bea4005ab55
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2025-04-02 11:15:45 +0200

    Update 4-array.py

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -4,6 +4,7 @@ Module that selects the last 10 rows from 'High' and 'Close' columns
 of a DataFrame and returns them as a NumPy array.
 """
 
+
 def array(df):
     """
     Selects the last 10 rows of the 'High' and 'Close' columns from df
@@ -15,7 +16,7 @@ def array(df):
     Returns:
         The selected values as a NumPy array.
     """
-    # Select the last 10 rows of 'High' and 'Close' columns
+    # Select the last 10 rows of 'High' and 'Close'
     last_ten = df[['High', 'Close']].tail(10)
     # Convert to a NumPy array
     return last_ten.to_numpy()
