ad6e9dc43376250cfd0ace445ce0c6a6a57184f3
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2025-04-05 15:32:59 +0200

    Update 13-analyze.py

--- pipeline/pandas/13-analyze.py
+++ pipeline/pandas/13-analyze.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Module that computes descriptive statistics for all columns except 'Timestamp'.
+Module that computes descriptive statistics for all columns
+except 'Timestamp'.
 """
 
 
@@ -12,7 +13,8 @@ def analyze(df):
         df: A DataFrame-like object, including a 'Timestamp' column.
 
     Returns:
-        A new DataFrame with the descriptive statistics of the non-'Timestamp' columns.
+        A new DataFrame with the descriptive statistics
+        of the non-'Timestamp' columns.
     """
     # Drop the 'Timestamp' column and compute descriptive stats
     stats = df.drop(columns=['Timestamp']).describe()
