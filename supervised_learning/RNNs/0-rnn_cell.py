68d576bd95a7ffc1eab8a9372c78d9a1e2fad77c
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2025-04-11 09:46:29 +0200

    Update 0-rnn_cell.py

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -1,58 +1,54 @@
-
 #!/usr/bin/env python3
 """
-    Module to create class RNNCell that represents a cell of a simple RNN
+Basic RNN Cell
 """
+
 import numpy as np
-
-
-def softmax(x):
-    """
-        compute softmax activation
-    """
-    max_x = np.amax(x, 1).reshape(x.shape[0], 1)  # Get the row-wise maximum
-    e_x = np.exp(x - max_x)  # For stability
-    return e_x / e_x.sum(axis=1, keepdims=True)
 
 
 class RNNCell:
     """
-        class that represent a cell of a simple RNN
+    This class represents a cell of a simple RNN
     """
+
     def __init__(self, i, h, o):
         """
-            class constructor
+        Initialize the RNN cell.
 
-        :param i: dimensionality of the data
-        :param h: dimensionality of the hidden state
-        :param o: dimensionality of the outputs
+        :Parameters:
+        - i (int): Dimensionality of the data.
+        - h (int): Dimensionality of the hidden state.
+        - o (int): Dimensionality of the outputs.
         """
-        i_h_concat = i + h
-        self.Wh = np.random.normal(size=(i_h_concat, h))
-        self.Wy = np.random.normal(size=(h, o))
-        self.bh = np.zeros(shape=(1, h))
-        self.by = np.zeros(shape=(1, o))
+        # Weigths initialized with a random normal distribution
+        self.Wh = np.random.randn(i + h, h)
+        self.Wy = np.random.randn(h, o)
+
+        # biases are initialized with zeros
+        self.bh = np.zeros((1, h))
+        self.by = np.zeros((1, o))
 
     def forward(self, h_prev, x_t):
         """
-            performs forward propagation for one time step
+        Perform forward propagation for one time step.
 
-        :param h_prev: ndarray, shape(m,h) previous hidden state
-        :param x_t: ndarray, shape(m,i) data input for cell
-            m: batche size for the data
+        :Parameters:
+        - h_prev (numpy.ndarray): Previous hidden state of shape (m, h).
+        - x_t (numpy.ndarray): Data input for the cell of shape (m, i).
 
-        :return: h_next, y
-            h_next: next hidden state
-            y: output of the cell
+        :Returns:
+        - h_next (numpy.ndarray): Next hidden state.
+        - y (numpy.ndarray): softmax-activated output of the cell.
         """
+        # Concatenate previous hiddent state and cell data input
+        concat_h_x = np.concatenate((h_prev, x_t), axis=1)
 
-        # concat x_t and h_prev
-        x_concat = np.concatenate((h_prev, x_t), axis=1)
+        # Calculate next hidden state using tanh activation function
+        h_next = np.tanh(np.dot(concat_h_x, self.Wh) + self.bh)
 
-        # next hidden state with tanh activation
-        h_next = np.tanh((x_concat @ self.Wh + self.bh))
+        # Calculate output using softmax activation function
+        y_linear = np.dot(h_next, self.Wy) + self.by
+        y = np.exp(y_linear) / np.exp(y_linear).sum(axis=1, keepdims=True)
 
-        # output with softmax activation
-        y = softmax(h_next @ self.Wy + self.by)
-
+        # Return both the hidden state and the softmax-activated output
         return h_next, y
