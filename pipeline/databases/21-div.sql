3016fe448eeb31fb1e87cf87bc9902a50218f28e
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2025-05-20 15:25:23 +0200

    Create 21-div.sql

---
+++ pipeline/databases/21-div.sql
@@ -0,0 +1,19 @@
+-- Function that divides (and return) two numbers
+DROP FUNCTION IF EXISTS SafeDiv;
+
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(
+    a INT,
+    b INT)
+    RETURNS FLOAT
+
+    BEGIN
+        IF b = 0 THEN
+            RETURN 0;
+        ELSE
+            RETURN a / b;
+        END IF;
+    END $$
+
+DELIMITER;
