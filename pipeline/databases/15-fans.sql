64967dd7d1dc2c8eaf0e89662a4fe1fa46eba5b4
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2025-05-20 15:11:16 +0200

    Update 15-fans.sql

--- pipeline/databases/15-fans.sql
+++ pipeline/databases/15-fans.sql
@@ -1,5 +1,14 @@
--- Script to rank countries by total number of fans (non-unique)
-SELECT origin, SUM(nb_fans) AS nb_fans
-FROM metal_bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
+-- 15-fans.sql
+
+-- Afficher le nombre total de fans par pays d'origine (origin)
+-- Ordonné par nombre de fans décroissant
+
+SELECT
+    origin,
+    SUM(nb_fans) AS nb_fans
+FROM
+    metal_bands
+GROUP BY
+    origin
+ORDER BY
+    nb_fans DESC;
