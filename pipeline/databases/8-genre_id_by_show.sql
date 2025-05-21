f8d2cc36ea07fced44274e88cab767b32ec3987e
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2025-05-21 14:22:27 +0200

    Update 8-genre_id_by_show.sql

--- pipeline/databases/8-genre_id_by_show.sql
+++ pipeline/databases/8-genre_id_by_show.sql
@@ -1,5 +1,8 @@
--- Script to list all shows with at least one genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+SELECT ts.title, tg.id genre_id
+from
+    tv_show_genres tsg
+    JOIN tv_shows ts on ts.id = tsg.show_id
+    JOIN tv_genres tg on tg.id = tsg.genre_id
+ORDER BY ts.title, tg.id ASC;
